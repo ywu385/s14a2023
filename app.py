8e706a8535621b89203f21eab78c15243bb1443a
John Wu <yuw385@g.harvard.com>
2023-06-28 23:30:48 -0700

    Adding datetime and usertimezone endpoints

--- app.py
+++ app.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!  Welcome to S14a2023</p>"
+
+@app.route("/datetime")
+# show the current date time in the server's timezone and another datetime in the UTC timezone
+
+@app.route("/usertimezone")
+#  third one for your own timezone with the location you are currently at
