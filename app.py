97e07e408362becda6d73801470caf254bc56c30
John Wu <yuw385@g.harvard.com>
2023-06-30 20:01:51 -0700

    Adding DateTime Route

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
-import pytz
+import pytz 
+import datetime as dt
+
+def tz_string(tz_obj):
+    return tz_obj.tzinfo.tzname(tz_obj)
+
 
 
 app = Flask(__name__)
@@ -10,6 +15,21 @@ def hello_world():
 
 @app.route("/datetime")
 # show the current date time in the server's timezone and another datetime in the UTC timezone
-def 
-@app.route("/usertimezone")
-#  third one for your own timezone with the location you are currently at
+def datetime():
+    time_now= dt.datetime.now() # creating current time 
+    sys_time = time_now.astimezone() # creating system time 
+    sys_tz = tz_string(sys_time)
+    
+    utc_time = dt.datetime.now(dt.timezone.utc) # creating utc time now
+
+    if sys_time == 'PDT':
+        my_time = sys_time
+    else:
+        my_time = dt.datetime.now(dt.timezone(dt.timedelta(hours=-7), 'PDT'))
+ 
+    # Display the timezones
+    print(f"""
+          System Timezone: {sys_tz} --- System Time: {sys_time}
+          UTC Timezone: {tz_string(utc_time)} --- UTC Time: {utc_time}
+          My Timezone: {tz_string(my_time)} --- My Time: {my_time}
+          """)
